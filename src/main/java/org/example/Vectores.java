e46db967ee5da5556e922c6a89f4826d6b1b93f1
A604\1DAW <edgarfer2205@gmail.com>
2024-11-06 20:56:38 +0100

    .

--- src/main/java/org/example/Vectores.java
+++ src/main/java/org/example/Vectores.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -12,72 +13,72 @@ public class Vectores {
         vector = new int[7];
 
         vector[2] = 7;
-        vector[5] = vector[2]+4;
+        vector[5] = vector[2] + 4;
 
-        System.out.println( vector[2] + " " + vector[5]);
-        for(int i= 0;i<=vector.length-1;i++){
+        System.out.println(vector[2] + " " + vector[5]);
+        for (int i = 0; i <= vector.length - 1; i++) {
             System.out.println(" " + vector[i] + " ");
 
         }
 
         String palabras[] = new String[3];
-        palabras [0] ="Pepe";
-        for(int i=0;i< palabras.length;i++) {
+        palabras[0] = "Pepe";
+        for (int i = 0; i < palabras.length; i++) {
             System.out.print(" " + palabras[i] + " ");
         }
     }
 
-    public void ejercicio1bateria(){
+    public void ejercicio1bateria() {
         Random aleatorio = new Random();
 
         int[] numeros = new int[8];
 
-        int suma=0;
+        int suma = 0;
 
-        for (int i = 0;i< numeros.length;i++){ //bucle para rellenar los vectores
+        for (int i = 0; i < numeros.length; i++) { //bucle para rellenar los vectores
 
             numeros[i] = aleatorio.nextInt(501);
 
-            suma+=numeros[i];
+            suma += numeros[i];
 
         }
-            System.out.println("La suma de los elementos es: " + suma); //Mostrarlos por pantalla
+        System.out.println("La suma de los elementos es: " + suma); //Mostrarlos por pantalla
 
     }
 
-    public void ejercicio2bateria(){
+    public void ejercicio2bateria() {
         Scanner teclado = new Scanner(System.in);
 
         int numeros[] = new int[5];
 
         System.out.println("Introduce los numeros a invertir: ");
 
-        for (int i = 0; i < numeros.length;i++) { //For para introducir numeros introducidos a mano a los vectores
+        for (int i = 0; i < numeros.length; i++) { //For para introducir numeros introducidos a mano a los vectores
 
             numeros[i] = teclado.nextInt();
 
         }
 
-        for(int i =numeros.length-1 ;i>=0; i--){ //Monstrar los vectores invertidos
+        for (int i = numeros.length - 1; i >= 0; i--) { //Monstrar los vectores invertidos
             System.out.println(" " + numeros[i] + " ");
         }
 
 
     }
 
-    public void ejercicio3bateria(){
+    public void ejercicio3bateria() {
         Scanner teclado = new Scanner(System.in);
         Random aleatorio = new Random();
 
-        int numero[]= new int[25];
+        int numero[] = new int[25];
 
         int suma = 0;
 
-       int numin = 0;
+        int numin = 0;
 
-       int contador = 0;
+        int contador = 0;
 
-        for (int i = 0;i< numero.length;i++){ //bucle para rellenar los vectores
+        for (int i = 0; i < numero.length; i++) { //bucle para rellenar los vectores
 
             numero[i] = aleatorio.nextInt(101);
 
@@ -85,8 +86,8 @@ public class Vectores {
         System.out.println("Dime un numero [0-100]: ");
         numin = teclado.nextInt();
 
-        for (int i = 0; i< numero.length; i++){ //for para comprobar cuantas veces se repite un numero introducido
-            if (numin == numero[i]){
+        for (int i = 0; i < numero.length; i++) { //for para comprobar cuantas veces se repite un numero introducido
+            if (numin == numero[i]) {
                 contador++;
             }
         }
@@ -94,4 +95,44 @@ public class Vectores {
 
     }
 
+    public void ejercicio5bateria() {
+
+        int numeros[] = {3, 5, 2, 1, 4};
+
+        int ultima_posicion = numeros[numeros.length - 1];
+
+        for (int i = numeros.length - 1; i >= 0; i--) {
+
+            if (i == 0) {
+
+                numeros[i] = ultima_posicion;
+
+            } else {
+
+                numeros[i] = numeros[i - 1];
+            }
+        }
+
+        for (int i = 0; i < numeros.length; i++) {
+
+            System.out.print(numeros[i]);
+
+        }
+
+
+    }
+
+    public void vectores2() {
+
+        String palabras[] = {"Juan", "Pepe", "Manolo", "Maria"};
+
+        for(String i :palabras){
+            System.out.println(i);
+        }
+        String textovector = Arrays.toString(palabras);
+        System.out.println( textovector);
+    }
+
+
 }
+
